2c5acf50ef9017d3bb2055104c5d2cbcc2549c35
Dustin Rouillard <me@dustin.sh>
2020-07-19 13:41:37 -0700

    Change out things for fastify version update

--- lib/modules/request.ts
+++ lib/modules/request.ts
@@ -1,19 +1,19 @@
 import fastifyPlugin from 'fastify-plugin';
 import { Server, IncomingMessage, ServerResponse } from 'http';
-import { FastifyRequest, FastifyReply, FastifyInstance, RegisterOptions } from 'fastify';
+import { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify';
 
 import { ParseIp } from './ip';
 import { ParseBytes } from './bytes';
 import { Log } from './logger';
 
-export const Logger = fastifyPlugin(function Logger(server: FastifyInstance<Server, IncomingMessage, ServerResponse>, _options: RegisterOptions<{}, {}, {}>, next?: () => void) {
-  server.addHook('onResponse', (request: FastifyRequest, reply: FastifyReply<ServerResponse>) => {
-    let ipAddress = ParseIp(request);
-    let method = request.raw.method;
-    let route = request.raw.url;
-    let code = reply.res.statusCode;
-    let rawSize = reply.getHeader('content-length');
-    let size = ParseBytes(Number(rawSize));
+export const Logger = fastifyPlugin(function Logger(server: FastifyInstance<Server, IncomingMessage, ServerResponse>, _, next?: () => void) {
+  server.addHook('onResponse', (request: FastifyRequest, reply: FastifyReply) => {
+    const ipAddress = ParseIp(request);
+    const method = request.raw.method;
+    const route = request.raw.url;
+    const code = reply.statusCode;
+    const rawSize = reply.getHeader('content-length');
+    const size = ParseBytes(Number(rawSize));
 
     Log(`${method} ${route} - ${code} - ${reply.getResponseTime().toFixed(2)}ms (${size}) - IP: ${ipAddress}`);
   });
@@ -21,8 +21,8 @@ export const Logger = fastifyPlugin(function Logger(server: FastifyInstance<Serv
   if (next) next();
 });
 
-export const Missing = fastifyPlugin(function Missing(server: FastifyInstance<Server, IncomingMessage, ServerResponse>, _options: RegisterOptions<{}, {}, {}>, next?: () => void) {
-  server.setNotFoundHandler((_request: FastifyRequest, reply: FastifyReply<ServerResponse>) => {
+export const Missing = fastifyPlugin(function Missing(server: FastifyInstance<Server, IncomingMessage, ServerResponse>, _, next?: () => void) {
+  server.setNotFoundHandler((_request: FastifyRequest, reply: FastifyReply) => {
     reply.code(404).send({ error: true, code: 'route_not_found' });
   });
 
--- lib/modules/response.ts
+++ lib/modules/response.ts
@@ -1,16 +1,16 @@
 import { FastifyReply } from 'fastify';
 
-export function Success(reply: FastifyReply<{}>, status: number, data?: any): void {
+export function Success(reply: FastifyReply, status: number, data?: any): void {
   reply.code(status).send({ success: true, data });
   return;
 }
 
-export function Failed(reply: FastifyReply<{}>, status: number, code: string, data?: any): void {
+export function Failed(reply: FastifyReply, status: number, code: string, data?: any): void {
   reply.code(status).send({ error: true, code, data });
   return;
 }
 
-export function Catch(reply: FastifyReply<{}>, data?: any): void {
+export function Catch(reply: FastifyReply, data?: any): void {
   reply.code(500).send({ error: true, code: 'catch', data });
   return;
 }
