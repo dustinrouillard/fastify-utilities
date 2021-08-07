2a388f4b8f6fa90e646f855799a34f8943e283bd
Dustin Rouillard <code@dstn.to>
2021-08-07 10:52:48 -0600

    Adding configurable logger options

--- lib/modules/logger.ts
+++ lib/modules/logger.ts
@@ -1,4 +1,26 @@
-import chalk from 'chalk';
+import chalk, { Color } from 'chalk';
+
+interface LoggerConfig {
+  timestampColor: typeof Color;
+  logColor: typeof Color;
+  errorColor: typeof Color;
+  debugColor: typeof Color;
+  disableColors: boolean;
+  debugAnnotation: string;
+}
+
+let LoggerConfig: LoggerConfig = {
+  timestampColor: 'gray',
+  logColor: 'green',
+  errorColor: 'redBright',
+  debugColor: 'blueBright',
+  disableColors: false,
+  debugAnnotation: 'dstn'
+};
+
+export function SetConfig(new_config: Partial<LoggerConfig>): void {
+  LoggerConfig = { ...LoggerConfig, ...new_config };
+}
 
 export function Log(...optionalParams: any[]) {
   let mainContent = optionalParams[0];
@@ -7,7 +29,11 @@ export function Log(...optionalParams: any[]) {
   let currentTime = new Date();
   let timestamp = `${currentTime.toTimeString().split(' ').slice(0, 2).join('-')} ${currentTime.toLocaleDateString().replace(/\//g, '-')}`;
 
-  console.log(`${chalk.gray(`[${timestamp}]`)} : ${chalk.yellow('L')} : ${chalk.green(mainContent)}`, ...optionalParams);
+  const ts = LoggerConfig.disableColors ? `[${timestamp}]` : `${chalk[LoggerConfig.timestampColor](`[${timestamp}]`)}`;
+  const sep = LoggerConfig.disableColors ? 'L' : `${chalk.blueBright('L')}`;
+  const content = LoggerConfig.disableColors ? mainContent : `${chalk[LoggerConfig.logColor](mainContent)}`;
+
+  console.log(`${ts} : ${sep} : ${content}`, ...optionalParams);
 }
 
 export function Error(...optionalParams: any[]) {
@@ -17,11 +43,15 @@ export function Error(...optionalParams: any[]) {
   let currentTime = new Date();
   let timestamp = `${currentTime.toTimeString().split(' ').slice(0, 2).join('-')} ${currentTime.toLocaleDateString().replace(/\//g, '-')}`;
 
-  console.error(`${chalk.gray(`[${timestamp}]`)} : ${chalk.red('E')} : ${chalk.redBright(mainContent)}`, ...optionalParams);
+  const ts = LoggerConfig.disableColors ? `[${timestamp}]` : `${chalk[LoggerConfig.timestampColor](`[${timestamp}]`)}`;
+  const sep = LoggerConfig.disableColors ? 'E' : `${chalk.red('E')}`;
+  const content = LoggerConfig.disableColors ? mainContent : `${chalk[LoggerConfig.errorColor](mainContent)}`;
+
+  console.error(`${ts} : ${sep} : ${content}`, ...optionalParams);
 }
 
 export function Debug(...optionalParams: any[]) {
-  if (!process.env.DEBUG || !process.env.DEBUG.includes('dustin:*')) return;
+  if (!process.env.DEBUG || !process.env.DEBUG.includes(`${LoggerConfig.debugAnnotation}:*`)) return;
 
   let mainContent = optionalParams[0];
   optionalParams.shift();
@@ -29,5 +59,9 @@ export function Debug(...optionalParams: any[]) {
   let currentTime = new Date();
   let timestamp = `${currentTime.toTimeString().split(' ').slice(0, 2).join('-')} ${currentTime.toLocaleDateString().replace(/\//g, '-')}`;
 
-  console.log(`${chalk.gray(`[${timestamp}]`)} : ${chalk.magenta('D')} : ${chalk.blueBright(mainContent)}`, ...optionalParams);
+  const ts = LoggerConfig.disableColors ? `[${timestamp}]` : `${chalk[LoggerConfig.timestampColor](`[${timestamp}]`)}`;
+  const sep = LoggerConfig.disableColors ? 'D' : `${chalk.magenta('D')}`;
+  const content = LoggerConfig.disableColors ? mainContent : `${chalk[LoggerConfig.debugColor](mainContent)}`;
+
+  console.log(`${ts} : ${sep} : ${content}`, ...optionalParams);
 }
