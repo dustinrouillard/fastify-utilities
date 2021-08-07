98980c60080acc454015ba6070afe7949eb53449
Dustin Rouillard <code@dstn.to>
2021-08-07 13:39:15 -0600

    Add ignoredRoutes config for logger This will be breaking for logger, it's a function now

--- lib/modules/request.ts
+++ lib/modules/request.ts
@@ -6,20 +6,28 @@ import { ParseIp } from './ip';
 import { ParseBytes } from './bytes';
 import { Log } from './logger';
 
-export const Logger = fastifyPlugin(function Logger(server: FastifyInstance<Server, IncomingMessage, ServerResponse>, _, next?: () => void) {
-  server.addHook('onResponse', (request: FastifyRequest, reply: FastifyReply) => {
-    const ipAddress = ParseIp(request);
-    const method = request.raw.method;
-    const route = request.raw.url;
-    const code = reply.statusCode;
-    const rawSize = reply.getHeader('content-length');
-    const size = ParseBytes(Number(rawSize));
+interface LoggerConfig {
+  ignoredRoutes?: string[];
+}
 
-    Log(`${method} ${route} - ${code} - ${reply.getResponseTime().toFixed(2)}ms (${size}) - IP: ${ipAddress}`);
+export function Logger(config: LoggerConfig) {
+  return fastifyPlugin(function Logger(server: FastifyInstance<Server, IncomingMessage, ServerResponse>, _, next?: () => void) {
+    server.addHook('onResponse', (request: FastifyRequest, reply: FastifyReply) => {
+      const ipAddress = ParseIp(request);
+      const method = request.raw.method;
+      const route = request.raw.url;
+      const code = reply.statusCode;
+      const rawSize = reply.getHeader('content-length');
+      const size = ParseBytes(Number(rawSize));
+
+      if (config.ignoredRoutes?.includes(route as string)) return;
+
+      Log(`${method} ${route} - ${code} - ${reply.getResponseTime().toFixed(2)}ms (${size}) - IP: ${ipAddress}`);
+    });
+
+    if (next) next();
   });
-
-  if (next) next();
-});
+}
 
 export const Missing = fastifyPlugin(function Missing(server: FastifyInstance<Server, IncomingMessage, ServerResponse>, _, next?: () => void) {
   server.setNotFoundHandler((_request: FastifyRequest, reply: FastifyReply) => {
